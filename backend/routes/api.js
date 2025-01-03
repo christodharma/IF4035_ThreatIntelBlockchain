aa20946c8e11dc79e65616b68cdc41c84bdacacf
Darmodar <haidarakita249@gmail.com>
2025-01-03 20:32:24 +0700

    feat: add apikey validator

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -2,12 +2,15 @@ const express = require("express");
 const multer = require("multer");
 const router = express.Router();
 const { uploadFile, handleDownload } = require("../controllers/ipfsController");
-const handleMulterError = require("../middlewares/errorHandler");
+const handleMulterError = require("../middlewares/errorHandlerLimit");
+const apiKeyValidator = require("../middlewares/apiKeyValidator");
 
 const upload = multer({
   storage: multer.memoryStorage(),
   limits: { fileSize: 100 * 1024 * 1024 }, 
 });
+
+router.use(apiKeyValidator);
 
 router.post("/upload", upload.single("file"), handleMulterError, uploadFile);
 router.get("/download", handleDownload);
