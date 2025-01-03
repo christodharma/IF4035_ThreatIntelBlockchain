9e8ed862ac071ff3a74d816dc270a1fe74da2b95
christodharma <christodharma@gmail.com>
2025-01-03 23:33:24 +0700

    fix: backend localhost port

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,6 @@ const apiRoutes = require("./routes/api");
 app.use(express.json());
 app.use("/api", apiRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
